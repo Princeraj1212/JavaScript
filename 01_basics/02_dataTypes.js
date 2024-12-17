7e830c109689dbb538253fab44d278c0787a65e7
Prince Raj <93924796+Princeraj1212@users.noreply.github.com>
2024-12-17 09:21:29 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all Js code as newer version.
+
+// alert( 3 + 3 )  we are using nodejs, not browser . (node js me error show krega and browser me value print krke dega.)
+
+console.log ( 3 
+    + 
+    3) // code readability should be high
+
+console.log("Prince")
+
+let name = "prince"               //String
+let age =  20                     //int
+let isLoggedIn = false            //boolean
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (it is a representation of empty value) 
+// undefined => when value is not defined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);      // Object
